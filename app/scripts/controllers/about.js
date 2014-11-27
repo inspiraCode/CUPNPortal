da782c9d92b7a3788fc5566792c0a632bba93525
Alfredo Pacheco <apacheco@capsonic.com>
2014-11-26 22:48:42 -0700

    Yeoman structure applied

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cupnPortalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cupnPortalApp
+ */
+angular.module('cupnPortalApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cupnPortalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cupnPortalApp
+ */
+angular.module('cupnPortalApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
