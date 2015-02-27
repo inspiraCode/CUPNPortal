6c8c91a517c95b8e074dedaa8bd6acd4d8a8c1b9
JFigue27 <jaimefigueroa84@gmail.com>
2015-02-27 16:57:14 -0700

    Add icons for the careers and tabs, also fix some text in home page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('cupnPortalApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.tab='Aspirantes';
+    $scope.tab='OfertaAcademica';
     angular.element('#myCarousel').slick({
       autoplay: true,
       autoplaySpeed: 4000,
