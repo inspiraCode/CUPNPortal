32facc146d102c3d073e7776d2abeaefa757467a
JFigue27 <jaimefigueroa84@gmail.com>
2015-02-28 17:36:10 -0700

    -background added -main logo does not resize anymore. -fix: Collapse nav afater clicking on hyperlink. -Resize carrusel on home. -Carrusel working.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ngResize'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,17 +3,42 @@
 /**
  * @ngdoc function
  * @name cupnPortalApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the cupnPortalApp
+ * @description # MainCtrl Controller of the cupnPortalApp
  */
-angular.module('cupnPortalApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.tab='OfertaAcademica';
-    angular.element('#myCarousel').slick({
-      autoplay: true,
-      autoplaySpeed: 4000,
-      arrows: true,
-      touchMove: true
-    }).show();
-  });
+angular.module('cupnPortalApp').controller('MainCtrl',
+		function($scope, resize, $timeout, $location) {
+			$scope.go = function(path) {
+				if (path == '') {
+					if (angular.element(".navbar-toggle").is(':visible')) {
+						angular.element(".navbar-toggle").click();
+					}
+
+				}
+				if (path != $location.url()) {
+					$location.url(path);
+					if (angular.element(".navbar-toggle").is(':visible')) {
+						angular.element(".navbar-toggle").click();
+					}
+				}
+			};
+
+			$scope.tab = 'OfertaAcademica';
+			$timeout(function() {
+				resizeImages();
+				angular.element('#myCarousel').slick({
+					autoplay : true,
+					autoplaySpeed : 4000,
+					arrows : true,
+					touchMove : true
+				}).show();
+			}, 200);
+			$scope.$on('resize', function() {
+				resizeImages();
+			})
+		});
+function resizeImages() {
+	angular.element('#myCarousel').css('width',
+			angular.element('body').css('width'));
+	angular.element('#img1').css('width', angular.element('body').css('width'));
+	angular.element('#img2').css('width', angular.element('body').css('width'));
+}
